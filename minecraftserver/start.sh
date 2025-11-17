401fe26d38618878f000b4258cbdf17124475e87
KevinHekert <37347425+KevinHekert@users.noreply.github.com>
2025-11-17 21:32:58 +0100

    Features/flask config (#23)
    * Update symlinks
    
    * Version update (#20)
    
    * Story/flask config/001 add flask interface (#22)
    
    * Version update
    
    * Workdir aanpassen
    
    * Version

--- minecraftserver/start.sh
+++ minecraftserver/start.sh
@@ -6,6 +6,6 @@ cd /opt/flask
 echo "🚀 Starting Flask webserver on port ${FLASK_PORT} (waitress)..."
 waitress-serve --listen=0.0.0.0:${FLASK_PORT} app:app &
 
-cd /data
+cd /opt/bds
 echo "🎮 Starting Bedrock server..."
 exec /opt/bedrock-entry.sh "$@"

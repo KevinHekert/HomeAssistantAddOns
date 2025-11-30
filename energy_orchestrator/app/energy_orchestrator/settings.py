9bb2a8acc58d8ad35c07d8c51e16c59b011209c8
Kevin Hekert <kevin@hekert.net>
2025-11-30 12:24:05 +0100

    ALLOWED_HOSTS adjustments for energy_orchestrator

--- energy_orchestrator/app/energy_orchestrator/settings.py
+++ energy_orchestrator/app/energy_orchestrator/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "dev-only-secret-change-me")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition

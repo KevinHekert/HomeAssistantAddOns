2c5635c4e083bcf8863d731179e64653f7dbcb35
Kevin Hekert <kevin@hekert.net>
2025-11-30 12:20:15 +0100

    Testrun

--- energy_orchestrator/app/energy_orchestrator/settings.py
+++ energy_orchestrator/app/energy_orchestrator/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7tsee!*&m@w9z_k-yik0d5fdc9nceakz8u+w5%b@bfh+0xb5$r'
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "dev-only-secret-change-me")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'ui',
 ]
 
 MIDDLEWARE = [

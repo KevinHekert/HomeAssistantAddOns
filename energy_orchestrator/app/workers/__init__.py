db796732f8c140931327a77921a9d98f61a316b8
Kevin Hekert <kevin@hekert.net>
2025-11-30 21:43:58 +0100

    Worker Update

--- energy_orchestrator/app/workers/__init__.py
+++ energy_orchestrator/app/workers/__init__.py
@@ -1,4 +1,3 @@
-from .wind import start_wind_logging_worker
-from .outdoortemperature import start_temperature_logging_worker
+from .sensors import start_sensor_logging_worker
 
-__all__ = ["start_wind_logging_worker", "start_temperature_logging_worker"]
+__all__ = ["start_sensor_logging_worker"]

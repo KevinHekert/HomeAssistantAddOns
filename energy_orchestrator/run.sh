7709c6c028ad94065999e6804d7a68b8585f1ecf
Kevin Hekert <kevin@hekert.net>
2025-11-30 15:37:44 +0100

    Django -> Flask

--- energy_orchestrator/run.sh
+++ energy_orchestrator/run.sh
@@ -1,11 +1,6 @@
 #!/usr/bin/with-contenv bashio
 
-bashio::log.info "Starting Django Energy Orchestrator"
+bashio::log.info "Starting Energy Orchestrator (Flask)"
 
 cd /app
-
-# Eventueel migrations draaien (nu nog weinig effect)
-python3 manage.py migrate --noinput || bashio::log.warning "Migrations failed (ok for first dev iterations)"
-
-# Start Django dev server – prima voor interne add-on
-python3 manage.py runserver 0.0.0.0:8099
+exec python app.py
